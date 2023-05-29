21be1b3b294955b8db969616200269fee2f268b0
marialoudiaz <loumariadiaz@gmail.com>
2023-05-30 00:22:16 +0200

    css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // le resultat
   // la ou on envoie le resultat des operateurs
-  const [result, setResult]=useState([])
+  const [result, setResult]=useState(1200) //[]
 
   // fonction reset
   const reset=()=>{setResult([])}  
@@ -20,28 +20,35 @@ function App() {
     <div className="Background">
       <div className='Screen'>
          <div className='Result'>{result}</div>
+      </div>
           <div className='Buttons'>
-            <button>AC</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-            <button>/</button>
-            <button>*</button>
-            <button>-</button>
-            <button>+</button>
+            <div className='row'>
+              <button>AC</button>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+            </div>
+            <div className='row'>
+             <button>4</button>
+             <button>5</button>
+             <button>6</button>
+             <button>1</button> 
+            </div>
+            <div className='row'>
+             <button>2</button>
+             <button>3</button>
+             <button>0</button>
+             <button>.</button>
+            </div>
+            <div className='row'>
+             <button className='operator'>=</button>
+             <button className='operator'>/</button>
+             <button className='operator'>*</button>
+             <button className='operator'>-</button>
+             <button className='operator'>+</button>
+            </div>
           </div> 
       </div>
-     
-    </div>
   );
 }
 
