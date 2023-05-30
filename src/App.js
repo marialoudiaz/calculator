f19c4c797ca314b469167ef63294c8fbc1a23af6
marialoudiaz <loumariadiaz@gmail.com>
2023-05-30 13:18:01 +0200

    css

--- src/App.js
+++ src/App.js
@@ -18,36 +18,44 @@ function App() {
 
   return (
     <div className="Background">
-      <div className='Screen'>
-         <div className='Result'>{result}</div>
-      </div>
-          <div className='Buttons'>
-            <div className='row'>
-              <button>AC</button>
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
+          <div className='Display'>
+            <div className='Screen'>
+              <div className='Result'>{result}</div>
             </div>
-            <div className='row'>
-             <button>4</button>
-             <button>5</button>
-             <button>6</button>
-             <button>1</button> 
-            </div>
-            <div className='row'>
-             <button>2</button>
-             <button>3</button>
-             <button>0</button>
-             <button>.</button>
-            </div>
-            <div className='row'>
-             <button className='operator'>=</button>
-             <button className='operator'>/</button>
-             <button className='operator'>*</button>
-             <button className='operator'>-</button>
-             <button className='operator'>+</button>
-            </div>
-          </div> 
+              <div className='Buttons'>
+              <div className='rowMain'>
+              <div className='row'>
+                <button>AC</button>
+              </div>
+              <div className='row'>
+                  <button>7</button>
+                  <button>8</button>
+                  <button>9</button>
+              </div>
+              <div className='row'>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+              </div>
+              <div className='row'>
+                <button>1</button> 
+                <button>2</button>
+                <button>3</button>
+              </div>
+              <div className='row'>
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
+              </div>
+              </div>
+              <div className='col'>
+                  <button className='operator'>/</button>
+                  <button className='operator'>*</button>
+                  <button className='operator'>-</button>
+                  <button className='operator'>+</button>
+              </div>
+        </div>
+        </div>
       </div>
   );
 }
