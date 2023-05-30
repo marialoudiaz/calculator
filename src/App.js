3c71b286a2c19b2e461833ceff340c7e5d6135bf
marialoudiaz <loumariadiaz@gmail.com>
2023-05-30 18:40:27 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
 
   // le resultat
   // la ou on envoie le resultat des operateurs
-  const [result, setResult]=useState(1200) //[]
+  const [result, setResult]=useState([]) //[] 
+  
+  const updateArray = (value) => {setResult([...result, value]);};
 
   // fonction reset
   const reset=()=>{setResult([])}  
 
   //fonction qui calcule
-  const calculate =()=>{result = eval(result.join(''))}
+  const calculate =()=>{
+    let answer = [...result]
+    let resultValue = eval(answer.join(''))
+    setResult([resultValue.toFixed(4)]) // reajoute le resultat dans une array pour continuer calcul
   
-  // chaque chiffre et opérateur est ajouté dans result (onClick) [...result, le truc ajouté]
-  // calculate donne le resultat
+  }
 
+ 
   return (
     <div className="Background">
           <div className='Display'>
@@ -25,34 +30,34 @@ function App() {
               <div className='Buttons'>
               <div className='rowMain'>
               <div className='row'>
-                <button>AC</button>
+                <button onClick={reset}>AC</button>
               </div>
               <div className='row'>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
+                  <button onClick={()=>updateArray(7)}>7</button>
+                  <button onClick={()=>updateArray(8)}>8</button>
+                  <button onClick={()=>updateArray(9)}>9</button>
               </div>
               <div className='row'>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
+                <button onClick={()=>updateArray(4)}>4</button>
+                <button onClick={()=>updateArray(5)}>5</button>
+                <button onClick={()=>updateArray(6)}>6</button>
               </div>
               <div className='row'>
-                <button>1</button> 
-                <button>2</button>
-                <button>3</button>
+                <button onClick={()=>updateArray(1)}>1</button> 
+                <button onClick={()=>updateArray(2)}>2</button>
+                <button onClick={()=>updateArray(3)}>3</button>
               </div>
               <div className='row'>
-                <button>0</button>
-                <button>.</button>
-                <button>=</button>
+                <button onClick={()=>updateArray(0)}>0</button>
+                <button onClick={()=>updateArray('.')}>.</button>
+                <button onClick={()=>calculate()}>=</button>
               </div>
               </div>
               <div className='col'>
-                  <button className='operator'>/</button>
-                  <button className='operator'>*</button>
-                  <button className='operator'>-</button>
-                  <button className='operator'>+</button>
+                  <button className='operator' onClick={()=>updateArray('/')}>/</button>
+                  <button className='operator' onClick={()=>updateArray('*')}>*</button>
+                  <button className='operator' onClick={()=>updateArray('-')}>-</button>
+                  <button className='operator' onClick={()=>updateArray('+')}>+</button>
               </div>
         </div>
         </div>
